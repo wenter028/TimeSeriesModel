653a78c27be3e0290e3e36c27814cbc6fda18f1a
Wenter <wenterwu028@gmail.com/>
2022-07-13 14:45:29 +0800

    basic time series by using R

--- basic_time_series_model.py
+++ basic_time_series_model.py
@@ -1,9 +1,17 @@
+import numpy as np
+
 from helper import *
 import statsmodels.tsa.stattools as ts
 from statsmodels.stats.diagnostic import acorr_ljungbox
 import statsmodels.api as sm
-
 from arch import arch_model
+
+import torch
+
+##R
+from rpy2.robjects import r, pandas2ri,numpy2ri
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
 
 ##get the data processing from original data
 class DataProcessing:
@@ -60,8 +68,8 @@ class DataProcessing:
         return train_data, test_data
 
 
-##ARIMA and GARCH
-class Basic_TS:
+##ARIMA and GARCH by using python
+class Basic_TS_Py:
     def __init__(self, train_data, test_data):
         self.train_data = train_data
         self.test_data = test_data
@@ -165,18 +173,162 @@ class Basic_TS:
         plt.tight_layout()
         plt.show()
 
+##ARIMA and GARCH by using R
+class Basic_TS_R:
+    def __init__(self, train_data, test_data):
+        self.train_data = train_data
+        self.test_data = test_data
+
+        self.arima_R = importr('forecast')
+        self.garch_R = importr('rugarch')
+
+        self.arima_coefficients = None
+        self.garch_coefficients = None
+
+    @staticmethod
+    def _get_stationary(data):
+        if ts.adfuller(data)[1] <= 0.05:
+            if ts.kpss(data)[1] >= 0.05:
+                print('data stationary')
+                return True
+
+        print('data not stationary')
+        return False
+
+    @staticmethod
+    def _white_noise(data):
+        if acorr_ljungbox(data, lags=[10]).values[0][1] >= 0.05:
+            print('data WN')
+            return True
+
+        print('data not Wn')
+        return False
+
+    @staticmethod
+    def _plot_acf(data):
+        sm.graphics.tsa.plot_acf(data, lags=20)
+
+    def arima_fit(self, max_ar=8, max_ma=8):
+        pandas2ri.activate()
+
+        arima_model = self.arima_R.auto_arima(self.train_data.values, max_p=max_ar, max_q=max_ma, ic="bic",
+                                              seasonal=False, trace=False)
+        p, q = arima_model[6][0], arima_model[6][1]
+        resid = arima_model[7]
+
+        self.arima_coefficients = {'p': arima_model[0][:p], 'q': arima_model[0][p:p + q], 'resid': resid}
+        print('arima fit done')
+        pandas2ri.deactivate()
+
+    def arima_test(self):
+        resid = self.arima_coefficients['resid']
+        if self._white_noise(resid):
+            print('arima fit mean well')
+            if self._white_noise(abs(resid)):
+                print('volatility cluster fit well')
+            else:
+                print('volatility cluster need to fit')
+        else:
+            print('arima fit mean bad')
+
+        # sm.qqplot(resid, line='q', fit=True)
+        # self._plot_acf(resid)
+        # self._plot_acf(abs(resid))
+        # plt.hist(resid,figsize=(15, 10))
+        # plt.show()
+
+    def garch_fit(self):
+        pandas2ri.activate()
+        p = len(self.arima_coefficients['p'])
+        q = len(self.arima_coefficients['q'])
+        garch_model = self.garch_R.ugarchspec(
+            mean_model=robjects.r(f'list(armaOrder = c({p},{q}))'),
+            variance_model=robjects.r('list(model= "eGARCH",garchOrder=c(1,1))'),
+            distribution_model='std'
+        )
+
+        garch_fitted = self.garch_R.ugarchfit(
+            spec=garch_model,
+            data=train_data.values,
+            out_sample=10
+        )
+
+        numpy2ri.deactivate()
+        garch_result = garch_fitted.slots['fit']
+        mu = garch_result[9][0]
+        resid = garch_result[8]
+
+        self.garch_coefficients = {'mu': mu, 'p': garch_result[9][1:p + 1], 'q': garch_result[9][p + 1:p + q + 1],
+                                   'resid': resid}
+
+        pandas2ri.deactivate()
+        print('arima garch fit done')
+
+    def garch_test(self):
+        resid = self.garch_coefficients['resid']
+        if self._white_noise(resid):
+            if self._white_noise(abs(resid)):
+                print('arima garch fit well')
+
+        else:
+            print('arima garch fit bad')
+
+        # sm.qqplot(resid, line='q', fit=True)
+        # self._plot_acf(resid)
+        # self._plot_acf(abs(resid))
+        plt.hist(resid)
+        plt.show()
+
+    def predict(self, data, model_type='GARCH'):
+        if model_type == 'GARCH':
+
+            mu = self.garch_coefficients['mu']
+            p = self.garch_coefficients['p']
+            q = self.garch_coefficients['q']
+            p_len = len(p)
+            q_len = len(q)
+
+            mse_list = []
+            for i in range(len(data.columns)):
+                resid = np.zeros(q_len)
+                test_dat = data.iloc[:, i]
+                pred_list = []
+
+                for j in range(max(p_len, q_len), len(test_dat)):
+                    pred = mu
+                    for p_index in range(p_len):
+                        pred += p[p_index] * (test_dat.iloc[j - p_index - 1] - mu)
+                    for q_index in range(q_len):
+                        pred += q[q_index] * resid[-q_index - 1]
+
+                    pred_list.append(pred)
+                    resid = np.append(np.delete(resid, 0), data.iloc[j] - pred)
+
+                mse_list.append(self.mse(test_dat.iloc[-len(pred_list):], pred_list))
+        return mse_list
+
+    def result(self):
+        return self.predict(self.test_data)
+
+    @staticmethod
+    def mse(data_original, data_predict):
+        dat = torch.tensor(data_original)
+        pre = torch.tensor(data_predict)
+        return torch.nn.MSELoss(reduction='sum')(dat, pre)
+
 
 if __name__ == '__main__':
     dp = DataProcessing(ALL_DATES)
     train_data = dp.data_for_arima()[0]
     test_data = dp.data_for_arima()[1]
 
-    basic_ts = Basic_TS(train_data,test_data)
+    basic_ts_r = Basic_TS_R(train_data,test_data)
 
-    basic_ts.arima_fit(8,8)
-    basic_ts.arima_test()
-    basic_ts.garch_fit()
-    basic_ts.garch_test()
+    basic_ts_r.arima_fit()
+    basic_ts_r.arima_test()
+    basic_ts_r.garch_fit()
+    basic_ts_r.garch_test()
+    print(basic_ts_r.result())
 
 
 
